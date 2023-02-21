fd77df01f0cdc9b171af41a50c995a0faef0a9d7
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-02-21 13:50:01 +0530

    Created Server Using gen_tcp

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-erlang_http_server
-=====
+# Erlang Http Server
+## A Simple HTTP Server Written In Erlang
 
-An OTP application
-
-Build
------
-
-    $ rebar3 compile
+# Usage
+```bash
+$ rebar3 compile
+$ rebar3 shell
+1> erlang_http_server:start(<port>).
+```
--- src/erlang_http_server.erl
+++ src/erlang_http_server.erl
@@ -1,1 +1,16 @@
 -module(erlang_http_server).
+-export([start/1]).
+
+start(Port) ->
+    io:fwrite("Server Listening On http://localhost:~p/", [Port]),
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {packet, 0}, {reuseaddr, true}, {active, true}]),
+    loop(ListenSocket).
+
+loop(ListenSocket) ->
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    spawn(fun() -> handle_connection(Socket) end),
+    loop(ListenSocket).
+
+handle_connection(Socket) ->
+    gen_tcp:send(Socket, "HTTP 1.1 200 OK\r\n\r\n\r\n<!DOCTYPE html><head><title>Erlang HTTP Server</title></head><body><h1>Hello, Erlang!</h1><br><a href='https://github.com/0xN1nja' target='_blank'>Abhimanyu Sharma</a></body></html>"),
+    gen_tcp:close(Socket).
